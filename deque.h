b7e3442da81664910605edd2b3ccf4ddcea5757b
Mikhail Mosyagin <mortie@mortie-debian.mortie.net>
2016-11-13 21:31:28 +0300

    first commit

---
+++ deque.h
@@ -0,0 +1,489 @@
+#pragma once
+
+#ifndef DEQUE_H
+#define DEQUE_H
+
+#include <iterator>
+
+template <typename data_type>
+class deque {
+
+    template<typename value_type, typename container_type, int move_direction>
+    class deque_iterator : public std::iterator<std::random_access_iterator_tag, value_type> {
+
+    typedef deque_iterator<value_type, container_type, move_direction> iterator;
+    typedef typename std::iterator_traits<iterator>::pointer pointer;
+    
+    private:
+
+        container_type *container;
+        size_t offset;
+
+        void move(int move_offset) {
+            offset += move_offset * move_direction;
+        }
+
+    public:
+
+        iterator& operator++() {
+            move(1);
+
+            return *this;
+        }
+
+        iterator operator++(int) {
+            iterator res = *this;
+
+            move(1);
+
+            return res;
+        }
+
+        iterator& operator--() {
+            move(-1);
+
+            return *this;
+        }
+
+        iterator operator--(int) {
+            iterator res = *this;
+
+            move(-1);
+
+            return res;
+        }
+
+        iterator& operator+=(int delta_offset) {
+            move(delta_offset);
+
+            return *this;
+        }
+
+        iterator operator+(int delta_offset) const {
+            iterator res = *this;
+            res += delta_offset;
+
+            return res;
+        }
+
+        iterator& operator-=(int delta_offset) {
+            move(-delta_offset);
+
+            return *this;
+        }
+
+        iterator operator-(int delta_offset) const {
+            iterator res = *this;
+            res -= delta_offset;
+
+            return res;
+        }
+
+        int operator-(const iterator &other) const {
+            return static_cast<int>(offset) - static_cast<int>(other.offset);
+        }
+
+        value_type& operator*() const {
+            return (*container)[offset];
+        }
+
+        value_type& operator[](const int delta_offset) const {
+            return *(*this + delta_offset);
+        }
+
+        value_type* operator->() const {
+            return &((*container)[offset]);
+        }
+
+        iterator& operator=(const iterator &other) {
+            container = other.container;
+            offset = other.offset;
+
+            return *this;
+        }
+
+        bool operator==(const iterator &other) const {
+            return container == other.container &&
+                offset == other.offset;
+        }
+
+        bool operator!=(const iterator &other) const {
+            return !(*this == other);
+        }
+
+        bool operator<(const iterator &other) const {
+            return offset < other.offset;
+        }
+
+        bool operator<=(const iterator &other) const {
+            return offset <= other.offset;
+        }
+
+        bool operator>(const iterator &other) const {
+            return offset > other.offset;
+        }
+
+        bool operator>=(const iterator &other) const {
+            return offset >= other.offset;
+        }
+
+
+
+        deque_iterator(container_type *container, size_t offset) : 
+            container(container), offset(offset) {}
+
+        deque_iterator(const iterator &other) {
+            *this = other;
+        }
+
+        deque_iterator() {}
+
+
+
+        virtual ~deque_iterator() {}
+
+    };
+
+private:
+
+    data_type* vector;
+    
+    size_t vector_size;
+
+    size_t vector_capacity;
+
+    size_t begin_offset;
+
+    size_t end_offset;
+
+
+
+    void resize_vector(size_t new_capacity);
+
+    inline void adapt_vector(size_t new_size);
+
+
+
+    inline size_t move_index(size_t index, int offset) const;
+
+    inline size_t next_index(size_t index) const;
+
+    inline size_t previous_index(size_t index) const;
+
+public:
+
+    typedef deque_iterator<data_type, deque<data_type>, 1> iterator;
+
+    typedef deque_iterator<const data_type, const deque<data_type>, 1> const_iterator;
+
+    typedef deque_iterator<data_type, deque<data_type>, -1> reverse_iterator;
+    
+    typedef deque_iterator<const data_type, const deque<data_type>, -1> const_reverse_iterator;
+
+
+
+    void push_back(const data_type& new_element);
+
+    void pop_back();
+
+    void push_front(const data_type& new_element);
+
+    void pop_front();
+
+
+
+    data_type& back();
+
+    const data_type& back() const;
+
+    data_type& front();
+
+    const data_type& front() const;
+
+
+
+    bool empty() const;
+
+    size_t size() const;
+
+
+
+    iterator begin();
+
+    const_iterator begin() const;
+
+    const_iterator cbegin() const;
+
+    iterator end();
+    
+    const_iterator end() const;
+
+    const_iterator cend() const;
+
+    reverse_iterator rbegin();
+
+    const_reverse_iterator rbegin() const;
+
+    const_reverse_iterator crbegin() const;
+
+    reverse_iterator rend();
+
+    const_reverse_iterator rend() const;
+
+    const_reverse_iterator crend() const;
+
+
+
+    data_type& operator[](const size_t index);
+
+    const data_type& operator[](const size_t index) const;
+
+    bool operator==(const deque<data_type> &other) const;
+
+
+
+    deque();
+
+    deque(const deque& other);
+
+    virtual ~deque();
+
+};
+
+template <typename data_type>
+void deque<data_type>::resize_vector(size_t new_capacity) {
+    data_type* new_vector = new data_type[new_capacity];
+
+    for (size_t offset = 0; offset < vector_size; ++offset) {
+        new_vector[offset] = vector[(begin_offset + offset) % vector_capacity];
+    }
+
+    if (vector) {
+        delete[] vector;
+    }
+
+    vector = new_vector;
+    vector_capacity = new_capacity;
+
+    begin_offset = 0;
+    end_offset = vector_size;
+
+    return;
+}
+
+template <typename data_type>
+inline void deque<data_type>::adapt_vector(size_t new_size) {
+    vector_size= new_size;
+
+    if (vector_size* 4 < vector_capacity && vector_capacity / 4 >= 4) {
+        resize_vector(vector_capacity / 4);
+    } else if (vector_size* 2 > vector_capacity) {
+        resize_vector(vector_capacity * 2);
+    }
+}
+
+template <typename data_type, typename value_type, typename container_type, int move_direction>
+typename deque<data_type>::template deque_iterator<value_type, container_type, 
+    move_direction> operator+(int offset, const typename deque<data_type>::template deque_iterator<
+    value_type, container_type, move_direction> &iterator) {
+    return iterator + offset;
+}
+
+template <typename data_type>
+inline size_t deque<data_type>::move_index(size_t index, int offset) const {
+    offset %= vector_capacity;
+    if (offset < 0) {
+        offset += vector_capacity;
+    }
+
+    return (index + offset) % vector_capacity;
+}
+
+template <typename data_type>
+inline size_t deque<data_type>::next_index(size_t index) const {
+    return move_index(index, 1);
+}
+
+template <typename data_type>
+inline size_t deque<data_type>::previous_index(size_t index) const {
+    return move_index(index, -1);
+}
+
+
+
+template <typename data_type>
+void deque<data_type>::push_back(const data_type& new_element) {
+    vector[end_offset] = new_element;
+    end_offset = next_index(end_offset);
+
+    adapt_vector(vector_size+ 1);
+}
+
+template <typename data_type>
+void deque<data_type>::pop_back() {
+    end_offset = previous_index(end_offset);
+
+    adapt_vector(vector_size- 1);
+}
+
+template <typename data_type>
+void deque<data_type>::push_front(const data_type& new_element) {
+    begin_offset = previous_index(begin_offset);
+    vector[begin_offset] = new_element;
+
+    adapt_vector(vector_size+ 1);
+}
+
+template <typename data_type>
+void deque<data_type>::pop_front() {
+    begin_offset = next_index(begin_offset);
+
+    adapt_vector(vector_size- 1);
+}
+
+
+
+template <typename data_type>
+data_type& deque<data_type>::back() {
+    return vector[previous_index(end_offset)];
+}
+
+template <typename data_type>
+const data_type& deque<data_type>::back() const {
+    return vector[previous_index(end_offset)];
+}
+
+template <typename data_type>
+data_type& deque<data_type>::front() {
+    return vector[begin_offset];
+}
+
+template <typename data_type>
+const data_type& deque<data_type>::front() const {
+    return vector[begin_offset];
+}
+
+
+
+template <typename data_type>
+bool deque<data_type>::empty() const {
+    return !vector_size;
+}
+
+template <typename data_type>
+size_t deque<data_type>::size() const {
+    return vector_size;
+}
+
+
+
+template <typename data_type>
+typename deque<data_type>::iterator deque<data_type>::begin() {
+    return typename deque<data_type>::iterator(this, 0);
+}
+
+template <typename data_type>
+typename deque<data_type>::const_iterator deque<data_type>::begin() const {
+    return typename deque<data_type>::const_iterator(this, 0);
+}
+
+template <typename data_type>
+typename deque<data_type>::const_iterator deque<data_type>::cbegin() const {
+    return typename deque<data_type>::const_iterator(this, 0);
+}
+
+template <typename data_type>
+typename deque<data_type>::iterator deque<data_type>::end() {
+    return typename deque<data_type>::iterator(this, vector_size);
+}
+
+template <typename data_type>
+typename deque<data_type>::const_iterator deque<data_type>::end() const {
+    return typename deque<data_type>::const_iterator(this, vector_size);
+}
+
+template <typename data_type>
+typename deque<data_type>::const_iterator deque<data_type>::cend() const {
+    return typename deque<data_type>::const_iterator(this, vector_size);
+}
+
+template <typename data_type>
+typename deque<data_type>::reverse_iterator deque<data_type>::rbegin() {
+    return typename deque<data_type>::reverse_iterator(this, vector_size - 1);
+}
+
+template <typename data_type>
+typename deque<data_type>::const_reverse_iterator deque<data_type>::rbegin() const {
+    return typename deque<data_type>::const_reverse_iterator(this, vector_size - 1);
+}
+
+template <typename data_type>
+typename deque<data_type>::const_reverse_iterator deque<data_type>::crbegin() const {
+    return typename deque<data_type>::const_reverse_iterator(this, vector_size - 1);
+}
+
+template <typename data_type>
+typename deque<data_type>::reverse_iterator deque<data_type>::rend() {
+    return typename deque<data_type>::reverse_iterator(this, -1);
+}
+
+template <typename data_type>
+typename deque<data_type>::const_reverse_iterator deque<data_type>::rend() const {
+    return typename deque<data_type>::const_reverse_iterator(this, -1);
+}
+
+template <typename data_type>
+typename deque<data_type>::const_reverse_iterator deque<data_type>::crend() const {
+    return typename deque<data_type>::const_reverse_iterator(this, -1);
+}
+
+
+template <typename data_type>
+data_type& deque<data_type>::operator[](const size_t index) {
+    return vector[move_index(begin_offset, index)];
+}
+
+template <typename data_type>
+const data_type& deque<data_type>::operator[](const size_t index) const {
+    return vector[move_index(begin_offset, index)];
+}
+
+template <typename data_type>
+bool deque<data_type>::operator==(const deque<data_type> &other) const {
+    return vector == other.vector;
+}
+
+
+
+template <typename data_type>
+deque<data_type>::deque() {
+    vector = nullptr;
+    vector_size= 0;
+    begin_offset = end_offset = 0;
+
+    resize_vector(4);
+}
+
+template <typename data_type>
+deque<data_type>::deque(const deque& other) {
+    vector = nullptr;
+
+    if (vector_capacity != other.vector_capacity) {
+        resize_vector(other.vector_capacity);
+    }
+
+    for (size_t offset = 0; offset < other.vector_size; ++offset) {
+        vector[offset] = other.vector[(other.begin_offset + offset) % other.vector_capacity];
+    }
+    vector_size= other.vector_size;
+
+    begin_offset = 0;
+    end_offset = vector_size;
+}
+
+template <typename data_type>
+deque<data_type>::~deque() {
+    delete[] vector;
+}
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,207 @@
+#include <ctime>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <gtest/gtest.h>
+#include "deque.h"
+
+template<typename data_type>
+std::ostream& operator<<(std::ostream& output, const deque<data_type> &to_print) {
+    for (auto it = to_print.begin(); it != to_print.end(); ++it) {
+        if (it != to_print.begin())
+            output << ' ';
+        output << *it;
+    }
+    return output;
+}
+
+class DequeTest : public ::testing::Test {
+
+protected:
+
+    static void popElements(deque<int> &container, size_t amount) {
+        while (amount--) {
+            container.pop_back();
+        }
+    }
+    
+    static void pushElements(deque<int> &container, size_t amount) {
+        while (amount--) {
+            container.push_back(1337);
+        }
+    }
+
+    static bool checkIfFunctionIsLinear(unsigned int *xValues, unsigned int *yValues, size_t range,
+            float tangentLimit) {
+        float tangent;
+        float maxTangent;
+        maxTangent = static_cast<float>(yValues[0]) / static_cast<float>(xValues[0]);
+
+        for (size_t index = 1; index < range; ++index) {
+            tangent = static_cast<float>(yValues[index]) / static_cast<float>(xValues[index]);
+
+            maxTangent = maxTangent < tangent ? tangent : maxTangent;
+        }
+
+        return maxTangent <= tangentLimit;
+    }
+
+    unsigned int measureOperationTime(void (*operation)(deque<int>&, size_t), deque<int> &firstArgument,
+            size_t secondArgument) {
+        clock_t startTime = clock();
+
+        operation(bigDeque, secondArgument);
+
+        clock_t result = clock() - startTime;
+
+        return static_cast<unsigned int>(result);
+    }
+
+    DequeTest() {
+        floatDeque.push_back(0.0f);
+        floatDeque.push_back(1.0f);
+        floatDeque.push_back(2.0f);
+
+        stringDeque.push_front("world");
+        stringDeque.push_front("hello");
+
+        for (int count = 0; count < 1000; ++count) {
+            bigDeque.push_back(count);
+        }
+    }
+
+    deque<int> intDeque;
+    deque<float> floatDeque;
+    deque<std::string> stringDeque;
+
+    deque<int> bigDeque;
+
+};
+
+TEST_F(DequeTest, SizeTest) {
+    EXPECT_TRUE(intDeque.empty());
+    EXPECT_FALSE(floatDeque.empty());
+    EXPECT_FALSE(stringDeque.empty());
+
+    EXPECT_EQ(intDeque.size(), 0);
+    EXPECT_EQ(floatDeque.size(), 3);
+    EXPECT_EQ(stringDeque.size(), 2);
+}
+
+TEST_F(DequeTest, RandomAccessTest) {
+    EXPECT_EQ(bigDeque.front(), 0);
+    EXPECT_EQ(bigDeque.back(), 999);
+
+    for (int count = 0; count < 1000; ++count) {
+        EXPECT_EQ(bigDeque[count], count);
+    }
+
+    EXPECT_EQ(stringDeque[0], "hello");
+    EXPECT_EQ(stringDeque[1], "world");
+
+    EXPECT_EQ(floatDeque[1], 1.0f);
+}
+
+TEST_F(DequeTest, AssignmentTest) {
+    for (int count = 0; count < 1000; ++count) {
+        bigDeque[999 - count] = count;
+    }
+
+    for (int count = 0; count < 1000; ++count) {
+        ASSERT_EQ(bigDeque[count], 999 - count);
+    }
+}
+
+TEST_F(DequeTest, PushTest) {
+    for (int count = 5; count <= 9; ++count) {
+        intDeque.push_back(count);
+    }
+
+    for (int count = 4; count >= 0; --count) {
+        intDeque.push_front(count);
+    }
+
+    for (int pointer = 0; pointer <= 9; ++pointer) {
+        ASSERT_EQ(intDeque[pointer], pointer);
+    }
+}
+
+TEST_F(DequeTest, PopTest) {
+    for (size_t count = 1; count <= 2; ++count) {
+        floatDeque.pop_back();
+    }
+    floatDeque.pop_front();
+
+    EXPECT_TRUE(floatDeque.empty());
+
+    stringDeque.pop_front();
+
+    ASSERT_EQ(stringDeque.size(), 1);
+
+    EXPECT_EQ(stringDeque[0], "world");
+}
+
+TEST_F(DequeTest, ComplexityTest) {
+    size_t testsCount = 16;
+    unsigned int popTimes[testsCount];
+    unsigned int pushTimes[testsCount];
+    unsigned int testSizes[testsCount] = {
+        900000, 500000, 200000, 100000,
+        90000, 50000, 20000, 10000,
+        9000, 5000, 2000, 1000,
+        900, 500, 200, 100
+    };
+
+    for (size_t count = 0; count < testsCount; ++count) {
+        pushTimes[count] = static_cast<int>(measureOperationTime(pushElements, intDeque, 
+                    static_cast<unsigned int>(testSizes[count])));
+        popTimes[count] = static_cast<int>(measureOperationTime(popElements, intDeque, 
+                    static_cast<unsigned int>(testSizes[count])));
+    }
+
+
+    float tangentLimit = 4.0f;
+    ASSERT_TRUE(checkIfFunctionIsLinear(testSizes, pushTimes, testsCount, tangentLimit));
+    ASSERT_TRUE(checkIfFunctionIsLinear(testSizes, popTimes, testsCount, tangentLimit));
+}
+
+TEST_F(DequeTest, IteratorTest) {
+    deque<int>::iterator iterator = bigDeque.begin();
+    deque<int>::iterator secondIterator;
+
+    secondIterator = iterator;
+
+    secondIterator += 10;
+    iterator += 10;
+
+    ASSERT_EQ(iterator, secondIterator);
+    ASSERT_EQ(*iterator, 10);
+
+    secondIterator += 10;
+    *secondIterator = *iterator;
+
+    ASSERT_NE(iterator, secondIterator);
+    ASSERT_EQ(*iterator, *secondIterator);
+
+    secondIterator -= 10;
+
+    ASSERT_EQ(iterator, secondIterator);
+    ASSERT_EQ(*iterator, *secondIterator);
+
+    deque<int>::reverse_iterator reverseIterator = bigDeque.rbegin();
+    deque<int>::reverse_iterator secondReverseIterator = bigDeque.rbegin();
+
+    reverseIterator += bigDeque.size() - 11;
+    secondReverseIterator += bigDeque.size() - 11;
+
+    ASSERT_EQ(*reverseIterator, *iterator);
+    ASSERT_EQ(reverseIterator, secondReverseIterator);
+
+    ASSERT_LT(bigDeque.begin(), bigDeque.end());
+    ASSERT_GT(bigDeque.rend(), bigDeque.rbegin());
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
